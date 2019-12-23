c836a8a68d4c73d5aee2155611b62b36cfdf8ac3
pieromarini <piero.marini21@gmail.com>
2019-12-22 20:36:34 -0500

    removing imgui.ini from source control

--- primal/primal/renderer/shader.cpp
+++ primal/primal/renderer/shader.cpp
@@ -10,15 +10,11 @@
 namespace primal {
 
   Shader::Shader(const std::string& vertexSrc, const std::string& fragmentSrc) {
-	// create an empty vertex shader handle
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-	// Send the vertex shader source code to GL
-	// Note that std::string's .c_str is NULL character terminated.
 	const GLchar* source = vertexSrc.c_str();
 	glShaderSource(vertexShader, 1, &source, 0);
 
-	// Compile the vertex shader
 	glCompileShader(vertexShader);
 
 	GLint isCompiled = 0;
@@ -27,7 +23,6 @@ namespace primal {
 	  GLint maxLength = 0;
 	  glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
 
-	  // The maxLength includes the NULL character
 	  std::vector<GLchar> infoLog(maxLength);
 	  glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
 
@@ -39,15 +34,11 @@ namespace primal {
 	  return;
 	}
 
-	// create an empty fragment shader handle
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-	// Send the fragment shader source code to GL
-	// Note that std::string's .c_str is NULL character terminated.
 	source = fragmentSrc.c_str();
 	glShaderSource(fragmentShader, 1, &source, 0);
 
-	// Compile the fragment shader
 	glCompileShader(fragmentShader);
 
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
@@ -55,13 +46,10 @@ namespace primal {
 	  GLint maxLength = 0;
 	  glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
 
-	  // The maxLength includes the NULL character
 	  std::vector<GLchar> infoLog(maxLength);
 	  glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
 
-	  // We don't need the shader anymore.
 	  glDeleteShader(fragmentShader);
-	  // Either of them. Don't leak shaders.
 	  glDeleteShader(vertexShader);
 
 	  PRIMAL_CORE_ERROR("{0}", infoLog.data());
@@ -69,33 +57,24 @@ namespace primal {
 	  return;
 	}
 
-	// Vertex and fragment shaders are successfully compiled.
-	// Now time to link them together into a program.
-	// Get a program object.
 	m_RendererID = glCreateProgram();
 	GLuint program = m_RendererID;
 
-	// Attach our shaders to our program
 	glAttachShader(program, vertexShader);
 	glAttachShader(program, fragmentShader);
 
-	// Link our program
 	glLinkProgram(program);
 
-	// Note the different functions here: glGetProgram* instead of glGetShader*.
 	GLint isLinked = 0;
-	glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
+	glGetProgramiv(program, GL_LINK_STATUS, static_cast<int*>(&isLinked));
 	if (isLinked == GL_FALSE) {
 	  GLint maxLength = 0;
 	  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
 
-	  // The maxLength includes the NULL character
 	  std::vector<GLchar> infoLog(maxLength);
 	  glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
 
-	  // We don't need the program anymore.
 	  glDeleteProgram(program);
-	  // Don't leak shaders either.
 	  glDeleteShader(vertexShader);
 	  glDeleteShader(fragmentShader);
 
@@ -104,7 +83,6 @@ namespace primal {
 	  return;
 	}
 
-	// Always detach shaders after a successful link.
 	glDetachShader(program, vertexShader);
 	glDetachShader(program, fragmentShader);
   }
