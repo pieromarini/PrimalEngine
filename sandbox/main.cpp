648feea1ecac3282b32cf8eb774bd9020428226c
Piero Marini <piero.marini21@gmail.com>
2024-04-18 16:58:07 -0500

    chore: remove imgui and spdlog. replace glfw with sdl3

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,12 +1,5 @@
 #include "primal.h"
 
 int main() {
-	auto app = primal::Application();
-
-	try {
-		app.run();
-	} catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
+	test();
 }
---
+++ src/primal.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+inline void test() {
+	std::cout << "hello\n";
+}
