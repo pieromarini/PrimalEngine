4963a77e3876af5dd8bd9abc0ac4462f01cb9fc6
Piero Marini <piero.marini21@gmail.com>
2025-02-27 20:57:29 -0500

    chore: set immediate mode for testing

--- src/platform/vulkan/vulkan_renderer.cpp
+++ src/platform/vulkan/vulkan_renderer.cpp
@@ -303,7 +303,7 @@ void VulkanRenderer::createSwapchain(uint32_t width, uint32_t height) {
 																	//.use_default_format_selection()
 																	.set_desired_format(VkSurfaceFormatKHR{ .format = m_swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
 																	// use vsync present mode
-																	.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+																	.set_desired_present_mode(VK_PRESENT_MODE_IMMEDIATE_KHR)
 																	.set_desired_extent(width, height)
 																	.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
 																	.build()
